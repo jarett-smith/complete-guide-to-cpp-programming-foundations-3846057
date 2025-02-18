3a24a0a2281179158643eba87013c43126b4cf47
jarett-smith <143127755+jarett-smith@users.noreply.github.com>
2025-02-18 01:54:41 +0000

    Adding Video Work

--- src/Ch02/02_08b/CodeDemo.cpp
+++ src/Ch02/02_08b/CodeDemo.cpp
@@ -5,14 +5,21 @@
 #include <iostream>
 #include <cstdint>
 
-enum asset_type {texture, sound, animation, script};
+enum class asset_type {texture, sound, animation, script}; 
+// texture is 0, sound is 1, etc.
+// the above act pretty much like global variables
+
+enum class menu_section{background, music, sound, controls, texture};
+
+// Issue: both enums include texture and sound and they correspond to different place values. E.g. sound could be 2 or 3
 
 int main(){
-    int asset_value;
+    int sound = 8;
+    asset_type asset_value;
 
-    asset_value = sound;
+    asset_value = asset_type::sound;
 
-    std::cout << "asset_value = " << asset_value << std::endl;
+    std::cout << "asset_value = " << (int) asset_value << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
