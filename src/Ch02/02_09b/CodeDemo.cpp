f367105d5fad6189f575298ac7ffaba5070d9771
jarett-smith <143127755+jarett-smith@users.noreply.github.com>
2025-02-18 02:02:49 +0000

    Adding Video Work

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -5,10 +5,24 @@
 #include <iostream>
 #include <string>
 
-enum class character_role {protagonist, antagonist, sidekick, npc};
+enum class character_role {protagonist, antagonist, sidekick, npc}; // these are basically options for levels of a categorical variable
+
+struct game_character { // This essentially sets up the structure of a data frame
+    std::string name;
+    int level;
+    character_role role;
+};
 
 int main(){
-    
+    game_character buddy;
+    buddy.name = "Tony";
+    buddy.level = 10;
+    buddy.role = character_role::sidekick;
+
+    std::cout << buddy.name << " is a level " << buddy.level << " character with the role of " << 
+    (int) buddy.role << "." << std::endl;
+    // Note: the (int) cast is required because the character_role class is an enum
+
     std::cout << std::endl << std::endl;
     return 0;
 }
